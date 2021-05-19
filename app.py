a2bdb65159e123f7b741fa96228ee100a9490b5c
FuMiRiO <50404762+FuMiRiO@users.noreply.github.com>
2021-05-20 04:01:02 +0700

    :rocket: :+1: :100:

--- app.py
+++ app.py
@@ -4,29 +4,57 @@ Mainly use for receive Line bot webhooks.
 """
 from flask import Flask, request
 from linebot import *
+from linebot.models import *
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi('<Channel access token>')
-handler = WebhookHandler('<Channel secret>')
+access_token = "<chanel access token>"
+secret = "<chanel secret>"
+line_bot_api = LineBotApi(channel_access_token=access_token)
+handler = WebhookHandler(channel_secret=secret)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # body = request.get_data(as_text=True)
-    # print(body)
-    req = request.get_json(silent=True, force=True)
-    intent = req["queryResult"]["intent"]["displayName"]
-    text = req['originalDetectIntentRequest']['payload']['data']['message']['text']
-    id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
-    disname = line_bot_api.get_profile(id).display_name
-    
-    print('id = ' + id)
-    print('name = ' + disname)
-    print('text = ' + text)
-    print('intent = ' + intent)
-
+    body = request.get_json(silent=True, force=True)
+    retrieveString = body["events"][0]['message']['text']
+    replyToken = body["events"][0]['replyToken']
+    print("Msg : {0}\nReplyToken : {1}".format(retrieveString, replyToken))
+    sendReplyMsg(tokens=replyToken)
     return 'OK'
+
+
+def sendReplyMsg(tokens):
+    template_msg = {
+        "type": "buttons",
+        "thumbnailImageUrl": "https://miro.medium.com/max/3016/0*5RLp-IJkJC6dLHvV.jpg",
+        "title": "Test",
+        "text": "Test",
+        "actions": [
+            {
+                "type": "message",
+                "label": "Action 1",
+                "text": "Action 1"
+            },
+            {
+                "type": "message",
+                "label": "Action 2",
+                "text": "Action 2"
+            },
+            {
+                "type": "message",
+                "label": "Action 3",
+                "text": "Action 3"
+            },
+            {
+                "type": "message",
+                "label": "Action 4",
+                "text": "Action 4"
+            }
+        ]
+    }
+    line_bot_api.reply_message(tokens, TemplateSendMessage(
+        template=template_msg, alt_text="Use Mobile Devices"))
+
 
 if __name__ == "__main__":
     app.run()
