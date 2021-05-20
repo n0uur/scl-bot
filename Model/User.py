d980fc7f8f881bb1a3a77d2933d4746383c5cbd8
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-20 22:02:27 +0700

    :rocket: add .env

--- Model/User.py
+++ Model/User.py
@@ -2,3 +2,14 @@
 Use to references and store state for Line Users.
 """
 
+class User:
+
+    users = []
+
+    def __init__(self):
+        
+        User.users.append(self)
+
+    @classmethod
+    def findUser(cls):
+        pass
--- app.py
+++ app.py
@@ -2,59 +2,78 @@
 Web Application Module
 Mainly use for receive Line bot webhooks.
 """
+
+import os
+from dotenv import load_dotenv
+
 from flask import Flask, request
 from linebot import *
 from linebot.models import *
 
+from Model.User import User
+
+################## INIT ##################
+
+load_dotenv()
+
 app = Flask(__name__)
-access_token = "<chanel access token>"
-secret = "<chanel secret>"
+access_token = os.environ.get("LINE_ACCESS_TOKEN")
+secret = os.environ.get("LINE_CHANNEL_SECRET")
 line_bot_api = LineBotApi(channel_access_token=access_token)
 handler = WebhookHandler(channel_secret=secret)
 
+################## #### ##################
+
+################## ROUTE #################
 
 @app.route("/callback", methods=['POST'])
 def callback():
     body = request.get_json(silent=True, force=True)
+    print(body)
     retrieveString = body["events"][0]['message']['text']
     replyToken = body["events"][0]['replyToken']
     print("Msg : {0}\nReplyToken : {1}".format(retrieveString, replyToken))
     sendReplyMsg(tokens=replyToken)
     return 'OK'
 
+@app.route("/test")
+def testClass():
+    user = User()
+    return "%d" % User.count()
+
+################## ##### #################
+
+######################################################
 
 def sendReplyMsg(tokens):
     template_msg = {
         "type": "buttons",
-        "thumbnailImageUrl": "https://miro.medium.com/max/3016/0*5RLp-IJkJC6dLHvV.jpg",
-        "title": "Test",
-        "text": "Test",
+        "thumbnailImageUrl": "https://inwfile.com/s-aa/qct4vq.jpg",
+        "title": "จัดการ Router : Router 1",
+        "text": "คุณสามารถจัดการ Router นี้ได้ โดยสามารถเลือกการทำงานได้ที่เมนูด้านล่าง",
         "actions": [
             {
                 "type": "message",
-                "label": "Action 1",
-                "text": "Action 1"
+                "label": "ตั้งค่า",
+                "text": "Router Config,"
+            },
+            {
+                "type":"postback",
+                "label":"ตั้งค่า",
+                "data":"routerid=1",
+                "text":"Config Router"
             },
             {
                 "type": "message",
-                "label": "Action 2",
-                "text": "Action 2"
-            },
-            {
-                "type": "message",
-                "label": "Action 3",
-                "text": "Action 3"
-            },
-            {
-                "type": "message",
-                "label": "Action 4",
-                "text": "Action 4"
+                "label": "Remote",
+                "text": "Remote"
             }
         ]
     }
     line_bot_api.reply_message(tokens, TemplateSendMessage(
         template=template_msg, alt_text="Use Mobile Devices"))
 
+######################################################
 
 if __name__ == "__main__":
     app.run()
