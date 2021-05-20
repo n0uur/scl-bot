2e1f19eaf7f8221dc6d63cdec262502090feca38
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-21 05:01:20 +0700

    :rocket: add controller mock

---
+++ SCLController.py
@@ -0,0 +1,12 @@
+"""
+Main controller of this program
+use to call function from other module (likely proxy :D)
+"""
+
+def check_router_status(router_id) -> bool:
+    pass
+
+def get_routers():
+    pass
+
+
