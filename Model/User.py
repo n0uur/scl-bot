96fc06d818abc6210a6b83b06f1407875f278a4a
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-20 23:44:29 +0700

    :rocket: implemented User model

--- Model/User.py
+++ Model/User.py
@@ -1,15 +1,32 @@
-"""User Model
+"""
+User Model
 Use to references and store state for Line Users.
 """
 
 class User:
 
+    STATE_NORMAL = 0
+    STATE_SETTING = 1
+    STATE_REMOTE = 2
+
     users = []
 
-    def __init__(self):
-        
-        User.users.append(self)
+    def __init__(self, user_id):
+
+        self.user_id = user_id
+        self.state = self.STATE_NORMAL
+
+        self.current_router = None # current router id
+        self.current_config = None # current config on router for SNMP module
+        self.is_waiting_for_config = False # waiting for input from user to config router
+
+        self.reply_token = None
+
+        self.users.append(self)
 
     @classmethod
-    def findUser(cls):
-        pass
+    def getUser(cls, user_id):
+        for user in cls.users:
+            if user.user_id == user_id:
+                return user # return user object
+        return User(user_id) # create new user object
