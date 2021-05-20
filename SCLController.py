e8b1e5eacc4ed7896c57de76fb6722dec0688dce
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-21 05:29:50 +0700

    :rocket: add pyping for testing router availability

--- SCLController.py
+++ SCLController.py
@@ -1,12 +1,19 @@
 """
 Main controller of this program
-use to call function from other module (likely proxy :D)
+central of function to call from another module (likely proxy ?)
 """
 
+import pyping
+
 def check_router_status(router_id) -> bool:
+    # get router ip :P
+    r = pyping.ping('localhost')
+    if r.ret_code == 0:
+        return True
+    return False
+
+def check_syslog_status():
     pass
 
 def get_routers():
     pass
-
-
