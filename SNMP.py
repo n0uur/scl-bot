bdecce9b22af9b6dc1e780b6bb4a2ae8ea835e47
hikariz01 <kittipak01@gmail.com>
2021-05-22 00:02:09 +0700

    :rocket: refactor snmp

--- SNMP.py
+++ SNMP.py
@@ -2,22 +2,21 @@
 SNMP Manager Module
 """
 
-import datetime
 import sys
-
 from pysnmp.hlapi import *
+import datetime
 
-COMMUNITY_CODE = "sclbot"
-RT_IP_ADDR = {"1": "10.0.15.7"}
+COMUNITY_CODE = 'sclbot'
+IP_ADDR_RT1 = '10.0.15.7'
 DICT_GET_STATUS = {"1": "up", "2": "down"}
 DICT_SET_STATUS = {"up": "1", "down": "2"}
 
 
-def getSNMP(oid):
+def getSNMP(ipAddress, communityCode, oid, port=161):
     try:
         iterator = getCmd(SnmpEngine(),
-                          CommunityData(COMMUNITY_CODE),
-                          UdpTransportTarget((RT_IP_ADDR["1"], 161)),
+                          CommunityData(communityCode),
+                          UdpTransportTarget((ipAddress, port)),
                           ContextData(),
                           ObjectType(ObjectIdentity(oid))
                           )
@@ -29,15 +28,15 @@ def getSNMP(oid):
 
         return result
     except:
-        print("Connection to router timeout!")
+        print('Connection to router time out!')
         sys.exit()
 
 
-def setSNMP(oid, value):
+def setSNMP(ipAddress, communityCode, oid, value, port=161):
     try:
         iterator = setCmd(SnmpEngine(),
-                          CommunityData(COMMUNITY_CODE),
-                          UdpTransportTarget((RT_IP_ADDR["1"], 161)),
+                          CommunityData(communityCode),
+                          UdpTransportTarget((ipAddress, port)),
                           ContextData(),
                           ObjectType(ObjectIdentity(oid), value)
                           )
@@ -49,56 +48,58 @@ def setSNMP(oid, value):
 
         return result
     except:
-        print("Connection to router timeout!")
+        print('Connection to router time out!')
         sys.exit()
 
 
-def getRouterHostName():
-    return getSNMP(".1.3.6.1.2.1.1.5.0")
+def getRouterHostName(ip, communityCode):
+    return getSNMP(ip, communityCode, '.1.3.6.1.2.1.1.5.0')
 
 
-def setRouterHostName():
+def setRouterHostName(ip, communityCode):
     newHostName = input("NewHostName: ")
-    return setSNMP(".1.3.6.1.2.1.1.5.0", newHostName)
+    return setSNMP(ip, communityCode, '.1.3.6.1.2.1.1.5.0', newHostName)
 
 
-def getRouterUptime():
-    routerUptime = getSNMP(".1.3.6.1.2.1.1.3.0")
+def getRouterUptime(ip, communityCode):
+    routerUptime = getSNMP(ip, communityCode, '.1.3.6.1.2.1.1.3.0')
     routerUptime = datetime.timedelta(seconds=int(routerUptime) / 100)
     return routerUptime
 
 
-def getInterfaceCount():
-    return int(getSNMP(".1.3.6.1.2.1.2.1.0"))
+def getInterfaceCount(ip, communityCode):
+    return int(getSNMP(ip, communityCode, '.1.3.6.1.2.1.2.1.0'))
 
 
-def getInterfaceDescr(index):
-    return getSNMP(".1.3.6.1.2.1.2.2.1.2.%d" % index)
+def getInterfaceDescr(ip, communityCode, index):
+    return getSNMP(ip, communityCode, ".1.3.6.1.2.1.2.2.1.2.%d" % index)
 
 
-def getInterfaceAdminStatus(index):
-    return DICT_GET_STATUS[getSNMP(".1.3.6.1.2.1.2.2.1.7.%d" % index)]
+def getInterfaceAdminStatus(ip, communityCode, index):
+    return DICT_GET_STATUS[getSNMP(ip, communityCode, ".1.3.6.1.2.1.2.2.1.7.%d" % index)]
 
 
-def setInterfaceAdminStatus():
+def setInterfaceAdminStatus(ip, communityCode):
     selectedInterface = input("Interface: ")
     interfaceStatus = input("Status: ")
-    return setSNMP(".1.3.6.1.2.1.2.2.1.7." + selectedInterface, Integer(DICT_SET_STATUS[interfaceStatus]))
+    return setSNMP(ip, communityCode, ".1.3.6.1.2.1.2.2.1.7." + selectedInterface, Integer(DICT_SET_STATUS[interfaceStatus]))
 
 
-def getInterfaceLineStatus(index):
-    return DICT_GET_STATUS[getSNMP(".1.3.6.1.2.1.2.2.1.8.%d" % index)]
+def getInterfaceLineStatus(ip, communityCode, index):
+    return DICT_GET_STATUS[getSNMP(ip, communityCode, ".1.3.6.1.2.1.2.2.1.8.%d" % index)]
 
 
-def getRouterSNMP(ipAddress, communityCode):
-    # setRouterHostName()
-    # setInterfaceAdminStatus()
-    print("RouterHostName:", getRouterHostName())
-    print("RouterUptime:", getRouterUptime())
-    for i in range(1, getInterfaceCount()):
-        print("RouterInterface {0}:{1}\n\tAdmin Status: {2}\n\tOper Status: {3}".format(
-            i, getInterfaceDescr(i), getInterfaceAdminStatus(i), getInterfaceLineStatus(i)))
+def getRouterSMMP(ipAddress, communityCode):
+    details = []
+    details.append("ชื่อ Router : " +
+                   getRouterHostName(ipAddress, communityCode))
+    details.append(("เวลาที่เปิดใช้งาน : " +
+                    str(getRouterUptime(ipAddress, communityCode))))
+    for i in range(1, getInterfaceCount(ipAddress, communityCode)):
+        details.append("Interface ที่ {0}:{1}\n\tAdmin Status: {2}\n\tOper Status: {3}".format(
+            i, getInterfaceDescr(ipAddress, communityCode, i), getInterfaceAdminStatus(ipAddress, communityCode, i), getInterfaceLineStatus(ipAddress, communityCode, i)))
+    print(*details)
 
-# for debugging
-if __name__ == "__main__":
-    getRouterSNMP("10.0.15.7")
+
+if __name__ == '__main__':
+    getRouterSMMP('10.0.15.7', 'sclbot')
