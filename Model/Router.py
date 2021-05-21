44a357fb7b11b6de25fd92179052e6f40cd453b5
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-21 23:15:39 +0700

    :rocket: add routers getter

--- Model/Router.py
+++ Model/Router.py
@@ -24,21 +24,31 @@ class Router:
         return host.is_alive
 
     @classmethod
+    def loadRouters(cls):
+        # todo : read routers.yml file
+        routers = None
+        with open("routers.yml") as file:
+            routers = yaml.load(file, Loader=yaml.FullLoader)['routers']
+            # print(routers)
+        if routers is None:
+            print("routers.yml not found! copy routers.yml.example to routers.yml and try again.")
+            return None
+
+        cls.routers = [
+            Router(router) for router in routers
+        ]
+
+    @classmethod
     def getRouter(cls, router_id):
         if cls.routers is None:
-            # todo : read routers.yml file
-            routers = None
-            with open("routers.yml") as file:
-                routers = yaml.load(file, Loader=yaml.FullLoader)['routers']
-                # print(routers)
-            if routers is None:
-                print("routers.yml not found! copy routers.yml.example to routers.yml and try again.")
-                return None
-
-            cls.routers = [
-                Router(router) for router in routers
-            ]
+            cls.loadRouters()
         for router in cls.routers:
             if router.name == router_id:
                 return router
         return None
+
+    @classmethod
+    def getRouters(cls):
+        if cls.routers is None:
+            cls.loadRouters()
+        return cls.routers
