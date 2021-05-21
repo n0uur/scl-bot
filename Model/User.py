fd6e3e1c4e39696dc99b27f532ef22543855eeba
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-22 04:03:39 +0700

    :rocket: just pep8

--- Model/User.py
+++ Model/User.py
@@ -3,8 +3,8 @@ User Model
 Use to references and store state for Line Users.
 """
 
-class User:
 
+class User:
     STATE_NORMAL = 0
     STATE_SETTING = 1
     STATE_REMOTE = 2
@@ -16,10 +16,10 @@ class User:
         self.user_id = user_id
         self.state = self.STATE_NORMAL
 
-        self.ssh_session = None # current router remote session
-        self.current_router = None # current router id
-        self.current_config = None # current config on router for SNMP module
-        self.is_waiting_for_config = False # waiting for input from user to config router
+        self.ssh_session = None  # current router remote session
+        self.current_router = None  # current router id
+        self.current_config = None  # current config on router for SNMP module
+        self.is_waiting_for_config = False  # waiting for input from user to config router
 
         self.reply_token = None
 
@@ -29,5 +29,5 @@ class User:
     def getUser(cls, user_id):
         for user in cls.users:
             if user.user_id == user_id:
-                return user # return user object
-        return User(user_id) # create new user object
+                return user  # return user object
+        return User(user_id)  # create new user object
