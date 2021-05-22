10f33f917583c1a27a7d7dfd0475b3057b92df31
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2021-05-22 18:53:27 +0700

    :rocket: use hostname from syslog packet

--- Syslog.py
+++ Syslog.py
@@ -1,23 +1,19 @@
 """
 Syslog receiver
 """
+
 import socketserver
-import logging
-from SNMP import getRouterHostName
 
 SYSLOG_PORT = 514
-SYSLOG_SERVER = "syslog server ip"
-ROUTER_IP = "router ip"
-COMMUNITY_CODE = "community code"
 
 
 class SyslogHandler(socketserver.BaseRequestHandler):
-    def handle(self) -> dict:
+    def handle(self):
         data = bytes.decode(self.request[0].strip())
         logData = data.split("*")[-1].split(": ")
         # print(logData)
         logTime = logData[0]
-        hostName = getRouterHostName(ROUTER_IP, COMMUNITY_CODE)
+        hostName = data.split("*")[0].split(":")[1].strip()
         logMessage = ""
         for i in logData[1:]:
             logMessage += i
@@ -26,19 +22,19 @@ class SyslogHandler(socketserver.BaseRequestHandler):
             "hostname": hostName,
             "message": logMessage
         }
-        print(logDataDict)
-        return logDataDict
+        # print(logDataDict)
+        # return logDataDict
 
 
 # for debugging
 
 def startSyslog():
-    try:
-        logging.warning(" * Starting Syslog Server on http://{}:{}/".format(SYSLOG_SERVER, SYSLOG_PORT))
-        server = socketserver.UDPServer((SYSLOG_SERVER, SYSLOG_PORT), SyslogHandler)
+    # try:
+        server = socketserver.UDPServer(("0.0.0.0", SYSLOG_PORT), SyslogHandler)
         server.serve_forever()
-    except Exception:
-        print("Something went wrong.. :(")
+    # except Exception:
+    #     print("Something went wrong.. :(")
+
 
 
 if __name__ == "__main__":
